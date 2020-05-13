bb8ed18fbc8e80eebf3422b3a0d229ef08c27672
Shion <shionchill@Shions-MacBook-Pro.local>
2020-05-14 01:15:53 +0700

    Updates

---
+++ precache-manifest.c9a2efee1079abd173cfa28290577808.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329d2b2c6f2bb517f7d67c6f693c6b4f",
+    "url": "/simple-weather/index.html"
+  },
+  {
+    "revision": "21827e246ee63120cf66",
+    "url": "/simple-weather/static/js/2.558c5057.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simple-weather/static/js/2.558c5057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce8b0b3f06e1ad3f947",
+    "url": "/simple-weather/static/js/3.e1c5102c.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/simple-weather/static/js/3.e1c5102c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568d8ec3a6f9e1a49872",
+    "url": "/simple-weather/static/js/4.7d2bbbfd.chunk.js"
+  },
+  {
+    "revision": "d7471cc17f510b9038ce",
+    "url": "/simple-weather/static/js/5.819f4646.chunk.js"
+  },
+  {
+    "revision": "0e05722a015edada5ba0",
+    "url": "/simple-weather/static/js/6.edd8a32c.chunk.js"
+  },
+  {
+    "revision": "13fbff77ef6a87a5abca",
+    "url": "/simple-weather/static/js/main.f0a5e4ae.chunk.js"
+  },
+  {
+    "revision": "277021631a41dc1d6515",
+    "url": "/simple-weather/static/js/runtime-main.cc0ccf80.js"
+  }
+]);

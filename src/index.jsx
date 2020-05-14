914f75df81ac09bb82c41c7f1ed5a0350d700885
Shion <shionchill@Shions-MacBook-Pro.local>
2020-05-14 11:45:15 +0700

    Implemented

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
-import 'config/i18n';
 
+import 'config/i18n';
 import Root from 'config/root';
 
 /* -------------------------------------------------------------------------- */
